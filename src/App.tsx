039b7054cc789267a8a064b8f198bc297ad0915a
PaulSamPS <psamoylenko2@gmail.com>
2022-02-27 03:40:55 +0500

    eslint

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,15 @@
 import React from 'react';
 import styles from './App.module.scss';
-import {H} from "./components/H/H";
-import {Button} from "./components/Button/Button";
-import {P} from "./components/P/P";
+import {H} from './components/H/H';
+import {Button} from './components/Button/Button';
+import {P} from './components/P/P';
 
-function App() {
+export const App = () => {
   return (
     <div className={styles.app}>
-      <H size={"h3"}>tt</H>
-        <Button>В корзину</Button>
-        <P size={"m"}>что-то написано</P>
+      <H size={'h3'}>tt</H>
+      <Button>В корзину</Button>
+      <P size={'m'}>что-то написано</P>
     </div>
   );
-}
-
-export default App;
+};
