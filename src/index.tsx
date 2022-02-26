bd988a13297d6512774a6698ee69ab67ed631c6e
PaulSamPS <psamoylenko2@gmail.com>
2022-02-27 04:15:00 +0500

    layout

--- src/index.tsx
+++ src/index.tsx
@@ -2,10 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.scss';
 import {App} from './App';
+import {Layout} from './layout/Layout';
 
 ReactDOM.render(
     <React.StrictMode>
-      <App />
+      <Layout>
+        <App />
+      </Layout>
     </React.StrictMode>,
     document.getElementById('root'),
 );
