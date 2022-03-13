3c075d0089ccce7f918e9232fb9c1612cd01cce0
PaulSamPS <psamoylenko2@gmail.com>
2022-03-14 04:36:16 +0500

    server changes

--- server/server.js
+++ server/server.js
@@ -1,10 +1,10 @@
 const jsonServer = require('json-server');
+const server = jsonServer.create();
 const products = require('./products.json');
 const slider = require('./slider.json');
 const nav = require('./nav.json');
-const server = jsonServer.create();
 const router = jsonServer.router({products, slider, nav});
-const middlewares = jsonServer.defaults({static: './build'});
+const middlewares = jsonServer.defaults({static: '../build'});
 
 const PORT = process.env.PORT || 3001;
 
