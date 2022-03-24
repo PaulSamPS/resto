e10fa9abf3b3bd580725459c87f63cb1c360d8d9
PaulSamPS <psamoylenko2@gmail.com>
2022-03-24 08:41:37 +0500

    mobile checkout

--- src/pages/Delivery/Checkout/Checckout.tsx
+++ src/pages/Delivery/Checkout/Checckout.tsx
@@ -1,29 +1,42 @@
 import React from 'react';
 import {DeliveryBlock} from '../../../components/DeliveryBlock/DeliveryBlock';
 import {Button, Flex, Span} from '../../../styles/components';
+import {device} from '../../../styles/breakpoints';
 import styled from 'styled-components';
 
 const Wrapper = styled.div`
   ${Flex};
-`;
 
-const Conditions = styled.div`
-  ${Flex};
+  @media only screen and ${device.tablet} {
+    display: grid;
+    grid-template-columns: 1fr;
+    grid-template-areas: 'btn' 'cond';
+    row-gap: 15px;
+  }
 `;
 
 const StyledLabel = styled.label`
   margin-right: 5px;
   color: var(--textWhite);
   font-size: 13px;
-  ${Flex};
   input {
     margin-right: 5px;
+  }
+  @media only screen and ${device.tablet} {
+    grid-area: cond;
+  }
+  @media only screen and ${device.mobileL} {
+    font-size: 10px;
   }
 `;
 
 const StyledSpan = styled(Span)`
   border-bottom: 1px solid var(--green);
   cursor: pointer;
+  grid-area: btn;
+  @media only screen and ${device.mobileL} {
+    font-size: 10px;
+  }
 `;
 
 const StyledBtn = styled(Button)``;
@@ -32,13 +45,11 @@ export const Checkout: React.FC = () : JSX.Element => {
   return (
     <DeliveryBlock width={100}>
       <Wrapper align={'center'} justify={'space-between'}>
-        <Conditions align={'center'}>
-          <StyledLabel align={'center'}>
-            <input type={'checkbox'}/>
+        <StyledLabel>
+          <input type={'checkbox'}/>
             Я согласен на обработку моих перс. данных в соответствии с
-          </StyledLabel>
           <StyledSpan size={13} color={'#72A479'}> Условиями</StyledSpan>
-        </Conditions>
+        </StyledLabel>
         <StyledBtn>Оформить заказ</StyledBtn>
       </Wrapper>
     </DeliveryBlock>
